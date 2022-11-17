d07ea9c8b3ea0bd4c4d5c05fef5d18b24b35d5bb
paimthaisla <thaisla.batista@aluno.senai.br>
2022-11-16 21:19:18 -0300

    Teste

---
+++ CadastrosGerais/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace CadastrosGerais
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.cadastramentoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.atualizaçãoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exclusãoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.avaliaçãoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.funcionáriosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.publicosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.privadosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.temporáriosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.terciáriosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.empresaGirassolToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.empresaBatatinhaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.empresaGemeridoasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.AccessibleRole = System.Windows.Forms.AccessibleRole.Cursor;
+            this.menuStrip1.BackColor = System.Drawing.Color.DarkCyan;
+            this.menuStrip1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(0, 0, 0, 0);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cadastramentoToolStripMenuItem,
+            this.atualizaçãoToolStripMenuItem,
+            this.exclusãoToolStripMenuItem,
+            this.avaliaçãoToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
+            this.menuStrip1.Size = new System.Drawing.Size(829, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // cadastramentoToolStripMenuItem
+            // 
+            this.cadastramentoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.funcionáriosToolStripMenuItem,
+            this.terciáriosToolStripMenuItem});
+            this.cadastramentoToolStripMenuItem.Name = "cadastramentoToolStripMenuItem";
+            this.cadastramentoToolStripMenuItem.Size = new System.Drawing.Size(100, 20);
+            this.cadastramentoToolStripMenuItem.Text = "Cadastramento";
+            // 
+            // atualizaçãoToolStripMenuItem
+            // 
+            this.atualizaçãoToolStripMenuItem.Name = "atualizaçãoToolStripMenuItem";
+            this.atualizaçãoToolStripMenuItem.Size = new System.Drawing.Size(80, 20);
+            this.atualizaçãoToolStripMenuItem.Text = "Atualização";
+            // 
+            // exclusãoToolStripMenuItem
+            // 
+            this.exclusãoToolStripMenuItem.Name = "exclusãoToolStripMenuItem";
+            this.exclusãoToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
+            this.exclusãoToolStripMenuItem.Text = "Exclusão";
+            // 
+            // avaliaçãoToolStripMenuItem
+            // 
+            this.avaliaçãoToolStripMenuItem.Name = "avaliaçãoToolStripMenuItem";
+            this.avaliaçãoToolStripMenuItem.Size = new System.Drawing.Size(70, 20);
+            this.avaliaçãoToolStripMenuItem.Text = "Avaliação";
+            // 
+            // funcionáriosToolStripMenuItem
+            // 
+            this.funcionáriosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.publicosToolStripMenuItem,
+            this.privadosToolStripMenuItem,
+            this.temporáriosToolStripMenuItem});
+            this.funcionáriosToolStripMenuItem.Name = "funcionáriosToolStripMenuItem";
+            this.funcionáriosToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.funcionáriosToolStripMenuItem.Text = "Funcionários ";
+            // 
+            // publicosToolStripMenuItem
+            // 
+            this.publicosToolStripMenuItem.Name = "publicosToolStripMenuItem";
+            this.publicosToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.publicosToolStripMenuItem.Text = "Públicos ";
+            // 
+            // privadosToolStripMenuItem
+            // 
+            this.privadosToolStripMenuItem.Name = "privadosToolStripMenuItem";
+            this.privadosToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.privadosToolStripMenuItem.Text = "Privados";
+            // 
+            // temporáriosToolStripMenuItem
+            // 
+            this.temporáriosToolStripMenuItem.Name = "temporáriosToolStripMenuItem";
+            this.temporáriosToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.temporáriosToolStripMenuItem.Text = "Temporários ";
+            // 
+            // terciáriosToolStripMenuItem
+            // 
+            this.terciáriosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.empresaGirassolToolStripMenuItem,
+            this.empresaBatatinhaToolStripMenuItem,
+            this.empresaGemeridoasToolStripMenuItem});
+            this.terciáriosToolStripMenuItem.Name = "terciáriosToolStripMenuItem";
+            this.terciáriosToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.terciáriosToolStripMenuItem.Text = "Terciários";
+            // 
+            // empresaGirassolToolStripMenuItem
+            // 
+            this.empresaGirassolToolStripMenuItem.Name = "empresaGirassolToolStripMenuItem";
+            this.empresaGirassolToolStripMenuItem.Size = new System.Drawing.Size(185, 22);
+            this.empresaGirassolToolStripMenuItem.Text = "Empresa Girassol";
+            // 
+            // empresaBatatinhaToolStripMenuItem
+            // 
+            this.empresaBatatinhaToolStripMenuItem.Name = "empresaBatatinhaToolStripMenuItem";
+            this.empresaBatatinhaToolStripMenuItem.Size = new System.Drawing.Size(185, 22);
+            this.empresaBatatinhaToolStripMenuItem.Text = "Empresa Solvados";
+            // 
+            // empresaGemeridoasToolStripMenuItem
+            // 
+            this.empresaGemeridoasToolStripMenuItem.Name = "empresaGemeridoasToolStripMenuItem";
+            this.empresaGemeridoasToolStripMenuItem.Size = new System.Drawing.Size(185, 22);
+            this.empresaGemeridoasToolStripMenuItem.Text = "Empresa Gemeridoas";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(829, 214);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cadastro Geral";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem cadastramentoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem funcionáriosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem publicosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem privadosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem temporáriosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem terciáriosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem empresaGirassolToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem empresaBatatinhaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem empresaGemeridoasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem atualizaçãoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exclusãoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem avaliaçãoToolStripMenuItem;
+    }
+}
+
